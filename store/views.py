b7c6d566de005ccb2c25e7884e308e9f32dd81f7
Ali Hamza <ch2953917@gmail.com>
2025-12-28 01:42:15 +0500

    wokring on product_details , add product , set home and product button , apply filters in views.py

--- store/views.py
+++ store/views.py
@@ -5,20 +5,22 @@ from .models import *
 
 # Create your views here.
 def index_view(request):
-    trending_items = Product.objects.all()[:8]
-    subcategory_filter = request.GET.get('subcategory')
-    if subcategory_filter:
+    trending_items = Product.objects.all()[:6]
+    mobile_subcategory_filter = request.GET.get('mobile_subcategory')
+    tablet_subcategory_filter = request.GET.get('tablet_subcategory')
+    
+    if mobile_subcategory_filter:
         mobile_phones = Product.objects.filter(
             category__name='mobile_phone',
-            subcategory__name=subcategory_filter
+            subcategory__name=mobile_subcategory_filter
         )[:6]
     else:
         mobile_phones = Product.objects.filter(category__name='mobile_phone')[:6]
     
-    if subcategory_filter:
+    if tablet_subcategory_filter:
         tablets = Product.objects.filter(
             category__name='tablet',
-            subcategory__name=subcategory_filter
+            subcategory__name=tablet_subcategory_filter
         )[:6]
     else:
         tablets = Product.objects.filter(category__name='tablet')[:6]
@@ -28,18 +30,19 @@ def index_view(request):
         'mobile_phones': mobile_phones, 
         'tablets': tablets,
         'sub_categories': SubCategory.objects.all(),
-        'selected_subcategory': subcategory_filter,
+        'selected_mobile_subcategory': mobile_subcategory_filter,
+        'selected_tablet_subcategory': tablet_subcategory_filter,
     }
     return render(request, 'index.html', context)
 
 def product_view(request):
     subcategory_filter = request.GET.get('subcategory')
-    products = Product.objects.all()
+    product = Product.objects.all()
     if subcategory_filter:
-        products = products.filter(subcategory__name=subcategory_filter)
+        product = product.filter(subcategory__name=subcategory_filter)
     
     context = {
-        'products': products,
+        'product': product,
         'sub_categories': SubCategory.objects.all(),
         'selected_subcategory': subcategory_filter,
     }
